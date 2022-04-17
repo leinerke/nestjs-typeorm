3c4e88e0df5d5f3a31d918668b4e6fbbc8ba8b9f
Leinerke Suarez <leinerke2000@gmail.com>
2022-04-17 18:59:23 -0500

    Crear, actualizar y eliminar.

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -51,22 +51,21 @@ export class ProductsController {
     return this.productsService.findOne(productId);
   }
 
-  // @Post()
-  // create(@Body() payload: CreateProductDto) {
-  //   // return {
-  //   //   message: 'accion de crear',
-  //   //   payload,
-  //   // };
-  //   return this.productsService.create(payload);
-  // }
-  //
-  // @Put(':id')
-  // update(@Param('id') id: string, @Body() payload: UpdateProductDto) {
-  //   return this.productsService.update(+id, payload);
-  // }
-  //
-  // @Delete(':id')
-  // delete(@Param('id') id: string) {
-  //   return this.productsService.remove(+id);
-  // }
+  @Post()
+  create(@Body() payload: CreateProductDto) {
+    return this.productsService.create(payload);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateProductDto,
+  ) {
+    return this.productsService.update(+id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(+id);
+  }
 }
