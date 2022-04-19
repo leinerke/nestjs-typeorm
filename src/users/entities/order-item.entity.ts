1a1363dab2347053a70a1e714872d6867407a8a8
Leinerke Suarez <leinerke2000@gmail.com>
2022-04-19 00:21:58 -0500

    Serializar.

--- src/users/entities/order-item.entity.ts
+++ src/users/entities/order-item.entity.ts
@@ -6,6 +6,7 @@ import {
   Column,
   ManyToOne,
 } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 import { Product } from '../../products/entities/product.entity';
 import { Order } from './order.entity';
@@ -15,12 +16,14 @@ export class OrderItem {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Exclude()
   @CreateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   createAt: Date;
 
+  @Exclude()
   @UpdateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -6,6 +6,8 @@ import {
   Entity,
   OneToMany,
 } from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
+
 import { Customer } from './customer.entity';
 import { OrderItem } from './order-item.entity';
 
@@ -14,12 +16,14 @@ export class Order {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Exclude()
   @CreateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
   })
   createAt: Date;
 
+  @Exclude()
   @UpdateDateColumn({
     type: 'timestamptz',
     default: () => 'CURRENT_TIMESTAMP',
@@ -29,6 +33,30 @@ export class Order {
   @ManyToOne(() => Customer, (customer) => customer.orders)
   customer: Customer;
 
+  @Exclude()
   @OneToMany(() => OrderItem, (item) => item.order)
   items: OrderItem[];
+
+  @Expose()
+  get products() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .map((item) => ({
+          ...item.product,
+          quantity: item.quantity,
+        }));
+    }
+    return [];
+  }
+
+  @Expose()
+  get total() {
+    if (this.items) {
+      return this.items
+        .filter((item) => !!item)
+        .reduce((total, item) => total + item.product.price * item.quantity, 0);
+    }
+    return 0;
+  }
 }
