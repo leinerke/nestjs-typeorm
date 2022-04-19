a78c15f03c2bb7fddaacaa9ae3addb358a166c70
Leinerke Suarez <leinerke2000@gmail.com>
2022-04-18 23:08:26 -0500

    Resolviendo la relación muchos a muchos personalizada en el controlador.

--- src/users/entities/customer.entity.ts
+++ src/users/entities/customer.entity.ts
@@ -4,10 +4,11 @@ import {
   Entity,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  OneToOne,
+  OneToOne, OneToMany,
 } from 'typeorm';
 
 import { User } from './user.entity';
+import { Order } from './order.entity';
 
 @Entity()
 export class Customer {
@@ -37,4 +38,7 @@ export class Customer {
 
   @OneToOne(() => User, (user) => user.customer, { nullable: true })
   user: User;
+
+  @OneToMany(() => Order, (order) => order.customer)
+  orders: Order[];
 }
